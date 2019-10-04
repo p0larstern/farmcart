f5be3ce1c7ef59ea0617f3f80b8c97aecfd8ec71
Dhruv Pal <41905327+p0larstern@users.noreply.github.com>
2019-10-04 23:20:31 +0530

    Update studenthome.php

--- studenthome.php
+++ studenthome.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="UTF-8" >
 	<title>HOME</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
